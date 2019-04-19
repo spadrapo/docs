8b49608d56635930c705fd64ce5b7c0441d1a602
Thiago Henrique da Silva <thsilva@gmail.com>
2019-04-19 15:13:21 -0300

    start creating a sandbox component

---
+++ src/WebDocs/wwwroot/components/sandbox/sandbox.ts
@@ -0,0 +1,32 @@
+﻿async function sandboxConstructor(el: HTMLElement, app: any): Promise<Sandbox> {
+    //Initialize
+    let instance: Sandbox = new Sandbox(el, app);
+    await instance.Initalize();
+    return (instance);
+}
+
+class Sandbox {
+    //Field
+    private _el: HTMLElement = null;
+    private _app: any;
+    private _sector: string = null;
+    //Properties
+    //Constructors
+    constructor(el: HTMLElement, app: any) {
+        this._el = el;
+        this._app = app;
+    }
+
+    public async Initalize(): Promise<void> {
+        this._sector = this._app._document.GetSector(this._el);
+        const elContent: HTMLDivElement = this.GetElementContent();
+        const content: string = $(elContent).html();
+        await this._app._functionHandler.ResolveFunctionWithoutContext(this._sector, 'UpdateData(html,' + content +')');
+        $(elContent).remove();
+    }
+
+    private GetElementContent(): HTMLDivElement {
+        return (<HTMLDivElement>this._el.children[2]);
+    }
+}
+
