fe31b5058a45166359f250f5aeb09a6ee7751f94
Thiago Henrique da Silva <thsilva@gmail.com>
2019-08-07 09:13:41 -0300

    add function PostDataitem

---
+++ src/WebDocs/Controllers/SampleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDocs.Models;
+
+namespace WebDocs.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class SampleController : Controller
+    {
+        public static ValueDateVM _valueDate = null;
+        [HttpGet]
+        public ValueDateVM GetValue()
+        {
+            if (_valueDate == null)
+            {
+                _valueDate = new ValueDateVM();
+                _valueDate.Date = DateTime.Now;
+                _valueDate.Value = "John Doe";
+            }
+            return (_valueDate);
+        }
+
+        [HttpPost]
+        public ValueDateVM SetValue([FromBody] ValueDateVM valueDate)
+        {
+            _valueDate = valueDate;
+            _valueDate.Date = DateTime.Now;
+            return (_valueDate);
+        }
+    }
+}
---
+++ src/WebDocs/Models/ValueDateVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDocs.Models
+{
+    public class ValueDateVM
+    {
+        public string Value { set; get; }
+        public DateTime Date { set; get; }
+    }
+}
